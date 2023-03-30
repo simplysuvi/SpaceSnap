bb712848ba96878c628cc7d97b7182b193b8b424
Suvrat Jain <suvijain@gmail.com>
2023-03-29 23:50:23 -0400

    Update spacesnap-app.py

--- spacesnap-app.py
+++ spacesnap-app.py
@@ -20,4 +20,4 @@ with st.spinner("Loading APOD..."):
     # Display the APOD information
     st.write(f"Date: {data['date']}")
     st.write(f"Title: {data['title']}")
-    st.write(f"Explanation: \n\n {data['explanation']}")
+    st.write(f"Explanation: \n {data['explanation']}")
